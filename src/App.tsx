334ae3709b3a753aec7372dca35e7b325f7043ea
Kai Zheng <kaizhengde@gmail.com>
2024-01-28 08:50:05 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Temporal</h1>
+      <h2>Linear Algebra</h2>
+
+    </>
+  )
+}
+
+export default App
