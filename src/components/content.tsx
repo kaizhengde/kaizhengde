db578790def455f4f5b3e771ae3afb4e80215e19
Kai Zheng <kaizhengde@gmail.com>
2024-01-28 12:58:59 +0100

    content version 1.1 (footer)

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,16 +3,21 @@ import styles from './content.module.css';
 interface ContentProps {
   title: string;
   children: React.ReactNode;
+  footnote?: string;
 }
 
 const Content: React.FC<ContentProps> = ({
   title,
   children,
+  footnote,
 }) => {
   return (
     <div className={styles.div}>
       <h4>{title}</h4>
       {children}
+      <div>
+        {footnote && <p className="footnote">{`* ${footnote}`}</p>}
+      </div>
     </div>
   );
 }
--- src/components/list-content.tsx
+++ src/components/list-content.tsx
@@ -1,19 +1,24 @@
 import Content from './content.tsx';
+import styles from './content.module.css';
 
 interface ListContentProps {
   title: string;
-  listItems: string[];
+  listItems: React.ReactNode[];
+  footnote?: string;
 }
 
 const ListContent: React.FC<ListContentProps> = ({
   title,
   listItems,
+  footnote,
 }) => {
   return (
-    <Content title={title}>
-      <ul>
+    <Content title={title} footnote={footnote}>
+      <ul className={styles.ul}>
         {listItems.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li className={styles.li} key={index}>
+            {item}
+          </li>
         ))}
       </ul>
     </Content>
