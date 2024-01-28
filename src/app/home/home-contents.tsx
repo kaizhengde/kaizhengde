c23788117b19852321279f7b2a218e03f04c7b3e
Kai Zheng <kaizhengde@gmail.com>
2024-01-28 13:34:04 +0100

    home version 1.1 (setup complete)

--- src/app/home/home-contents.tsx
+++ src/app/home/home-contents.tsx
@@ -5,11 +5,37 @@ const HomeContents = () => {
     <section className="section-content">
       <div className="div-layer-text">
         <ListContent
-          title="NICE BOOKS"
+          title="CURRENTLY"
+          listItems={[
+            <a href="">Linear Algebra (HS23 G-08)</a>,
+            <a href="">BSc Thesis</a>,
+          ]}
+        />
+
+        <ListContent
+          title="NICE QUOTE"
+          listItems={[
+            "You have power over your mind—not outside events. Realize this, and you will find strength.*"
+          ]}
+          footnote="Marcus Aurelius, Roman emperor (121-180 AD)"
+        />
+
+        <ListContent
+          title="WRITINGS"
           listItems={[
 
           ]}
-          footnote="currently reading"
+        />
+
+        <ListContent
+          title="NICE BOOKS"
+          listItems={[
+            "Meditations, Marcus Aurelius",
+            "赢家法则, 博多舍费尔",
+            "Trotzdem Ja zum Leben sagen, Viktor E. Frankl",
+            <a href="">Books 2024</a>,
+            <a href="">Books 2023</a>,
+          ]}
         />
 
         <ListContent
