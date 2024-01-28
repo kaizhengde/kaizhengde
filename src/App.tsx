587642f56574cf1d8e8151b7109c8dc04c866d10
Kai Zheng <kaizhengde@gmail.com>
2024-01-28 13:39:19 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 import Home from '@/app/home/home.tsx';
+import LinearAlgebra from '@/app/linear-algebra/linear-algebra.tsx';
 
 const App = () => {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/linear-algebra" element={<LinearAlgebra />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
